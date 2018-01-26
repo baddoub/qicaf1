093bf0b3b5af214a148c3966e672e57b667220a9
badrbaddou <badr.baddou@gmail.com>
2018-01-26 19:39:24 +0100

    Le submit marche mais l'affichage est dégueu.

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,33 +3,35 @@ package controllers
 import javax.inject._
 
 import models.RestaurantList
+import play.api.data.Form
 import play.api.mvc._
 
-/**
-  * This controller creates an `Action` to handle HTTP requests to the
-  * application's home page.
-  */
+import scala.util.Random
+
 @Singleton
-class HomeController @Inject()(messagesAction: MessagesActionBuilder, cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(cc: MessagesControllerComponents) extends MessagesAbstractController(cc)  {
 
 
-  val restaurants: _root_.models.RestaurantList = RestaurantList("Italian", "Moroccan", "French", "Spanish")
+  var restaurants: _root_.models.RestaurantList = RestaurantList("Italian", "Moroccan", "French", "Spanish")
 
 
-  val postCall: _root_.play.api.mvc.Call = routes.HomeController.index()
+  val postCall: _root_.play.api.mvc.Call = routes.HomeController.display()
 
-  /**
-    * Create an Action to render an HTML page.
-    *
-    * The configuration in the `routes` file means that this method
-    * will be called when the application receives a `GET` request with
-    * a path of `/`.
-    */
-  def index = messagesAction { implicit request: MessagesRequest[AnyContent] =>
+  def index = Action { implicit request: MessagesRequest[AnyContent] =>
     Ok(views.html.index(restaurants, RestaurantForm.form, postCall))
   }
 
-  //  def restaurantList = Action {  implicit request: MessagesRequest[AnyContent] =>
-  //      Ok(views.html.index(restaurants, RestaurantForm.form, postUrl))
-  //  }
+  def display = Action { implicit request: MessagesRequest[AnyContent] =>
+    val errorFunction = { formWithErrors: Form[RestaurantForm.RestaurantsData] =>
+      Redirect(routes.HomeController.index()).flashing("Info" -> "Please fill in all fields.")
+    }
+    val successFunction = { data : RestaurantForm.RestaurantsData =>
+      val answer = List.newBuilder.+=(data.rest1).+=(data.rest2).+=(data.rest3).+=(data.rest4).result()
+      val random = new Random
+      Redirect(routes.HomeController.index()).flashing("Info" -> "You will eat at : ".concat(answer(random.nextInt(answer.length))))
+    }
+    val formValidationResult = RestaurantForm.form.bindFromRequest
+    formValidationResult.fold(errorFunction, successFunction)
+  }
+
 }
--- app/controllers/RestaurantForm.scala
+++ app/controllers/RestaurantForm.scala
@@ -6,7 +6,7 @@ object RestaurantForm {
   import play.api.data.Form
   import play.api.data.Forms._
 
-  case class RestaurantsData(rest1: String, rest2: String, rest3: String, rest4: String, rest5: String)
+  case class RestaurantsData(rest1: String, rest2: String, rest3: String, rest4: String)
 
 
   val form = Form(
@@ -14,8 +14,7 @@ object RestaurantForm {
       "rest1" -> nonEmptyText,
       "rest2" -> nonEmptyText,
       "rest3" -> nonEmptyText,
-      "rest4" -> nonEmptyText,
-      "rest5" -> nonEmptyText
+      "rest4" -> nonEmptyText
     )(RestaurantsData.apply)(RestaurantsData.unapply)
   )
 }
