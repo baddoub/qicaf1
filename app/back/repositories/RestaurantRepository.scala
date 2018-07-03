0aa6e95cea7a7727e83e0e3f1f12de20b88944ff
Sébastien Willmann <ext.swillmann@kyriba.com>
2018-07-03 16:56:33 +0200

    Separate restaurants for each user

--- app/back/repositories/RestaurantRepository.scala
+++ app/back/repositories/RestaurantRepository.scala
@@ -2,12 +2,14 @@ package back.repositories
 
 import models.Restaurant
 
+import scala.collection.mutable
+
 object RestaurantRepository {
-  private var restaurantList: Seq[Restaurant] = Seq()
+  private val restaurantMap: mutable.Map[String, Seq[Restaurant]] = mutable.Map.empty
 
-  def fetchRestaurants() = restaurantList
+  def fetchRestaurants(userId: String) = restaurantMap.getOrElse(userId, Seq())
 
-  def addRestaurant(restaurant: Restaurant): Unit = {
-    restaurantList :+= restaurant
+  def addRestaurant(userId: String, restaurant: Restaurant): Unit = {
+    restaurantMap += ((userId, restaurantMap.getOrElse(userId, Seq()) :+ restaurant))
   }
 }
--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -4,34 +4,39 @@ import javax.inject._
 
 import models.Restaurant
 import back.repositories.RestaurantRepository
+import org.pac4j.core.profile.{CommonProfile, ProfileManager}
+import org.pac4j.play.PlayWebContext
+import org.pac4j.play.scala.{Security, SecurityComponents}
+import org.pac4j.play.store.PlaySessionStore
 import play.api.data.Form
+import play.api.i18n.I18nSupport
 import play.api.mvc._
 
 import scala.util.Random
 
 @Singleton
-class HomeController @Inject()(cc: MessagesControllerComponents) extends MessagesAbstractController(cc)  {
+class HomeController @Inject()(sessionStore: PlaySessionStore, override val controllerComponents: SecurityComponents) extends Security[CommonProfile] with I18nSupport {
   val postCall: _root_.play.api.mvc.Call = routes.HomeController.display()
   val postAdd : _root_.play.api.mvc.Call = routes.HomeController.add()
 
-  def index = Action { implicit request: MessagesRequest[AnyContent] =>
-    Ok(views.html.index(RestaurantRepository.fetchRestaurants(), RestaurantForm.form, postCall, postAdd))
+  def index: Action[AnyContent] = Secure("Google2Client") { implicit request: Request[AnyContent] =>
+    Ok(views.html.index(RestaurantRepository.fetchRestaurants(getUserId(request)), RestaurantForm.form, postCall, postAdd))
   }
 
-  def display = Action { implicit request: MessagesRequest[AnyContent] =>
+  def display: Action[AnyContent] = Secure("Google2Client") { implicit request: Request[AnyContent] =>
     val random = new Random
-    val restaurantsList = RestaurantRepository.fetchRestaurants()
+    val restaurantsList = RestaurantRepository.fetchRestaurants(getUserId(request))
     Redirect(routes.HomeController.index()).flashing("Info" -> "You will eat at : ".concat(restaurantsList(random.nextInt(restaurantsList.length)).name))
   }
 
-  def add = Action { implicit request: MessagesRequest[AnyContent] =>
+  def add: Action[AnyContent] = Secure("Google2Client") { implicit request: Request[AnyContent] =>
     val errorFunction = { formWithErrors: Form[RestaurantForm.RestaurantsData] =>
       Redirect(routes.HomeController.index()).flashing("Info" -> "Error")
     }
 
     val successFunction = { data: RestaurantForm.RestaurantsData =>
       val restaurant = Restaurant(name = data.name)
-      RestaurantRepository.addRestaurant(restaurant)
+      RestaurantRepository.addRestaurant(getUserId(request), restaurant)
       Redirect(routes.HomeController.index()).flashing("info" -> "Restaurant added!")
     }
 
@@ -39,8 +44,14 @@ class HomeController @Inject()(cc: MessagesControllerComponents) extends Message
     formValidationResult.fold(errorFunction, successFunction)
   }
 
-  def listRestaurants = Action { implicit request: MessagesRequest[AnyContent] =>
+  def listRestaurants: Action[AnyContent] = Secure("Google2Client") { implicit request: Request[AnyContent] =>
     // Pass an unpopulated form to the template
-    Ok(views.html.index(RestaurantRepository.fetchRestaurants(), RestaurantForm.form, postCall, postAdd))
+    Ok(views.html.index(RestaurantRepository.fetchRestaurants(getUserId(request)), RestaurantForm.form, postCall, postAdd))
+  }
+
+  private def getUserId(request: Request[AnyContent]): String = {
+    val webContext = new PlayWebContext(request, sessionStore)
+    val profileManager = new ProfileManager[CommonProfile](webContext)
+    profileManager.get(true).orElseThrow(() => new RuntimeException).getTypedId
   }
 }
