a18644d3bc6cd2183173df4431b4fec642e05634
Sébastien Willmann <ext.swillmann@kyriba.com>
2018-07-04 16:25:06 +0200

    Install postgresql

--- app/back/repositories/RestaurantRepository.scala
+++ app/back/repositories/RestaurantRepository.scala
@@ -7,7 +7,7 @@ import scala.collection.mutable
 object RestaurantRepository {
   private val restaurantMap: mutable.Map[String, Seq[Restaurant]] = mutable.Map.empty
 
-  def fetchRestaurants(userId: String) = restaurantMap.getOrElse(userId, Seq())
+  def fetchRestaurants(userId: String): Seq[Restaurant] = restaurantMap.getOrElse(userId, Seq())
 
   def addRestaurant(userId: String, restaurant: Restaurant): Unit = {
     restaurantMap += ((userId, restaurantMap.getOrElse(userId, Seq()) :+ restaurant))
--- bootstrap-vagrant.sh
+++ bootstrap-vagrant.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/sh
-dnf -y install java-1.8.0-openjdk-devel
 curl https://bintray.com/sbt/rpm/rpm > bintray-sbt-rpm.repo
 mv bintray-sbt-rpm.repo /etc/yum.repos.d/
-dnf -y install sbt
+dnf -y install java-1.8.0-openjdk-devel sbt postgresql-server
+
+# Configure postgresql
+postgresql-setup --initdb --unit postgresql
+cp -v /vagrant/pg_hba.conf /var/lib/pgsql/data/pg_hba.conf
+systemctl enable postgresql
+systemctl start postgresql
+sudo -u postgres psql postgres <<EOF
+CREATE ROLE QICAF1 LOGIN;
+CREATE SCHEMA AUTHORIZATION QICAF1;
+EOF
